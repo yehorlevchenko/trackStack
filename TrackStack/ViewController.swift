2056be8f7aa349496bac1485e3fbf810d21e4576
Iegor Levchenko <mailyozzik@gmail.com>
2019-01-29 22:51:43 +0200

    Initial Commit

---
+++ TrackStack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrackStack
+//
+//  Created by Yehor Levchenko on 1/29/19.
+//  Copyright © 2019 Yehor Levchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
